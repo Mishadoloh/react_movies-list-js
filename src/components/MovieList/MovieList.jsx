4a210970f2e4f223b3064211da1bf01a675575bb
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-09-10 19:48:33 +0300

    add xyi

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,7 +2,7 @@ import { MovieCard } from '../MovieCard/MovieCard';
 import './MovieList.scss';
 
 export const MovieList = ({ movies }) => (
-  <div className="movies" data-cy="movies-container">
+  <div className="movies" data-cy="MovieList">
     {movies.map(movie => (
       <MovieCard key={movie.imdbId} movie={movie} />
     ))}
